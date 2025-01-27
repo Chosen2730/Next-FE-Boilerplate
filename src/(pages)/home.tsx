d4a0117f03af34f24f406bc012b4334359a75127
Chosen2730 <patiencesimoniseoluwa@gmail.com>
2025-01-27 21:31:44 +0100

    init codebase

--- src/(pages)/home.tsx
+++ src/(pages)/home.tsx
@@ -1,11 +1,16 @@
 "use client";
 import { Button, Typography } from "@material-tailwind/react";
-
+import "react-phone-number-input/style.css";
+import PhoneInput from "react-phone-number-input";
+import { useState } from "react";
 export default function Home() {
+	const [value, setValue] = useState<string | undefined>();
+
 	return (
 		<main className='flex h-screen flex-col items-center justify-between p-24'>
 			<Button>Hello World!</Button>
-			<Typography variant='h1'>Template Web App.</Typography>
+			<Typography variant='h1'>Template Web App</Typography>
+			<div className='w-full max-w-md'></div>
 		</main>
 	);
 }
